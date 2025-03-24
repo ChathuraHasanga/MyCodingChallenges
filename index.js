7bcf35f9c4d43ce6b501e4677b3e79fa255e8797
chathura722 <86885143+ChathuraHasanga@users.noreply.github.com>
2025-03-24 19:58:44 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function myFunction(){
-    var x = document.getElementById("mainnav");
-    if(x.className === "container"){
-        x.className += " responsive";
-    }else{
-        x.className == "container";
-    }
-}
